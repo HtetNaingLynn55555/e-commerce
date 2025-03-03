f50995bf25c1e430465704d3b474a995ab066d72
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-03-04 01:20:42 +0630

    add role controller demo

---
+++ src/controllers/Authorization/roleController.js
@@ -0,0 +1,29 @@
+let DB = require('../../models/role')
+
+let all = async(request, response, next)=>{
+
+}
+
+let create = async(request, response, next)=>{
+
+}
+
+let details = async(request, response, next)=>{
+
+}
+
+let update = async(request, response, next)=>{
+
+}
+
+let drop = async(request, response, next)=>{
+
+}
+
+module.exports = {
+    all,
+    create,
+    details,
+    update,
+    drop,
+}
