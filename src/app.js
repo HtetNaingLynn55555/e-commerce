103791332d1882615b2007e18c0c21cbaa056f7c
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-03-03 22:36:55 +0630

    mongoose connect with db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 let express = require('express')
+let mongoose = require('mongoose');
+
+
+
 
 
 
 let app = express();
 let PORT = process.env.PORT;
+
+// For request body upload
+app.use(express.json());
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
+
+
 app.listen(`${PORT}`, console.log(`App is running at port ${PORT}`))
